64a205c99fdca8fd945b8e1e50dcb32c55e29ddd
박은진 <@192.168.19.90>
2019-04-24 16:48:27 +0900

    CharString

---
+++ javastudy/src/numberstring/CharString.java
@@ -0,0 +1,15 @@
+package numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('지');
+		System.out.println("지니");
+		System.out.println("지니"+"입니다");
+		System.out.println("1"+"1");
+		System.out.println("Jee-Ni said \"Welcome programming world\"");
+		System.out.println("HTML\nCSS\nJavaScript\n");
+		
+	}
+
+}
