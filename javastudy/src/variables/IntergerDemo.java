6a9f941a587c034fd18054ef6e1eb950a3f2fda9
박은진 <@192.168.19.90>
2019-04-25 17:31:04 +0900

    Variables

--- javastudy/src/variables/IntergerDemo.java
+++ javastudy/src/variables/IntergerDemo.java
@@ -15,7 +15,26 @@ public class IntergerDemo {
 		
 		b = 2.1; 
 		System.out.println(b+1.1);
-
+		
+		String first = "coding"; 
+		System.out.println(first+" "+"everybody");
+		
+		String c, d;
+		c = "coding";
+		d = " everybody";
+		System.out.println(c+d);
+		
+		System.out.println(100 + 10);
+		System.out.println((100 + 10) / 10);
+		System.out.println(((100 + 10) / 10) - 10);
+		System.out.println((((100 + 10) / 10) - 10) * 10);
+		
+		int e = 100;
+		System.out.println(e + 10);
+		System.out.println((e+ 10) / 10);
+		System.out.println(((e + 10) / 10) - 10);
+		System.out.println((((e + 10) / 10) - 10) * 10);
+		
 	}
 
 }
