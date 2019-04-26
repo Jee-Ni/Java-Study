27abb7245c7f8f5c34eddd4ad5ffd01828e3489a
박은진 <@192.168.19.90>
2019-04-26 15:02:34 +0900

    Operator

---
+++ javastudy/src/operator/DivisionDemo.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class DivisionDemo {
+
+	public static void main(String[] args) {
+		int b = 10;
+        int c = 3;
+          
+        float d = 10.0F;
+        float e = 3.0F;
+          
+        System.out.println(b/c);
+        System.out.println(d/e);
+        System.out.println(b/e);
+
+	}
+
+}
