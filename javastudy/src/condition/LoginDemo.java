b9d37f8438fa55def6c4bcd3b59caf844a768d88
박은진 <@192.168.19.90>
2019-04-29 14:04:14 +0900

    Condition

--- javastudy/src/condition/LoginDemo.java
+++ javastudy/src/condition/LoginDemo.java
@@ -4,7 +4,7 @@ public class LoginDemo {
 
 	public static void main(String[] args) {
 		String id = args[0];
-        if(id.equals("jeeni")){
+        if(id.equals("jeeni")){  //run configurations> new launch> arguments
             System.out.println("right");
         } else {
             System.out.println("wrong");
