6c4b3d740711ec5d9790c2c9031ab24aaf0b8bf7
Jee-Ni <jeeni@okhome.id>
2019-05-02 20:59:49 +0900

    loop

---
+++ Java/src/loop/ForDemo.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<10){
+		    System.out.println("coding everybody"+(i+1)*2);
+		    i++;
+		}
+
+	}
+
+}
